2405c76383cd557dd6117c0b7d0f2dbb250eb26e
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-09-06 19:25:15 -0300

    build go

---
+++ build/precache-manifest.efd1e88f8039788b31da80194cb4e28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c015fb78d3fdda996d37c7ce4c9fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b31206ceb4395e091f1",
+    "url": "/static/css/main.f3d14423.chunk.css"
+  },
+  {
+    "revision": "c9f3d2d5bed6b4ef75dc",
+    "url": "/static/js/2.dbee464e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dbee464e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b31206ceb4395e091f1",
+    "url": "/static/js/main.788967dd.chunk.js"
+  },
+  {
+    "revision": "c198bcb1034fd1681a1f",
+    "url": "/static/js/runtime-main.9b1abea7.js"
+  }
+]);
